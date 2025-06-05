1a57ffa56d8faca8412b6e51edefd022fe9490ab
Felipe Romero <87445291+Pipe7993@users.noreply.github.com>
2025-06-05 16:24:42 -0400

    hay que cambiar el puerto de la bdd
    si

--- Ventas/frontend/src/App.tsx
+++ Ventas/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -12,7 +12,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function App() {
-  const [count, setCount] = useState(0);
   const location = useLocation();
 
   const hideNavbarRoutes = ["/login", "/register"];
@@ -29,28 +28,6 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
 
-      {!shouldHideNavbar && (
-        <>
-          <div>
-            <a href="https://www.terplac.cl/" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>Bienvenido a MundoPuertas!</p>
-          </div>
-          <p className="read-the-docs">
-            Click en la puerta para entrar al antiguo MundoPuertas
-          </p>
-        </>
-      )}
     </>
   );
 }
