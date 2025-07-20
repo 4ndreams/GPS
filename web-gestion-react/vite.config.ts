50de4fb67b5a34332ac55cf40e74a90b218f659d
LeandroCortesB <leandro.cortes2101@alumnos.ubiobio.cl>
2025-07-20 14:25:43 -0400

    Agregar hooks y servicios para la compra en bodega y la obtención de datos de compras y ventas totales filtradas

--- web-gestion-react/vite.config.ts
+++ web-gestion-react/vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src'
-    },
+      '@': '/src',
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@config': path.resolve(__dirname, 'src/config')
+    }
   },
   server: {
     port: 5173,
