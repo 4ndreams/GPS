d7bdd6b979a2d043d059769cca0fcbe8477a2056
Pipe7993 <feliperomero156.fr@gmail.com>
2025-07-23 03:57:55 +0000

    respaldar servidor 22/07

--- Ventas/frontend/src/services/api.js
+++ Ventas/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://146.83.198.35:1237",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
